8a7856534d0018ac3853c9a92083a4fc9ce7273e
Mariel <mariel.torres@uabc.edu.mx>
2025-09-12 19:54:45 -0700

    - creacion de ProfesorDAO - se modificó la password del root en persistence.xml

---
+++ persistencia/src/main/java/mx/desarrollo/dao/ProfesorDAO.java
@@ -0,0 +1,54 @@
+package mx.desarrollo.dao;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+import mx.desarollo.entity.Profesor;
+import mx.desarollo.persistence.AbstractDAO;
+
+import java.util.List;
+
+public class ProfesorDAO extends AbstractDAO<Profesor> {
+    private final EntityManager entityManager;
+
+    public ProfesorDAO(EntityManager em) {
+        super(Profesor.class);
+        this.entityManager = em;
+    }
+
+    public void guardar(Profesor profe) {
+        EntityTransaction tx = entityManager.getTransaction();
+        try {
+            tx.begin();
+            entityManager.createNativeQuery(
+                            "INSERT INTO profesor (Nombre,ApellidoP,ApellidoM,RFC) VALUES (?,?,?,?)")
+                    .setParameter(1, profe.getNombre())
+                    .setParameter(2, profe.getApellidoP())
+                    .setParameter(3, profe.getApellidoM())
+                    .setParameter(4, profe.getRfc())
+                    .executeUpdate();
+            tx.commit();
+        } catch (Exception e) {
+            if (tx.isActive()) {
+                tx.rollback();
+            }
+            throw e;
+        }
+    }
+
+    /*public void guardar(Profesor profe) {
+        EntityTransaction tx = entityManager.getTransaction();
+        try {
+            tx.begin();
+            entityManager.persist(profe);
+            tx.commit();
+        } catch (Exception e) {
+            if (tx.isActive()) tx.rollback();
+            throw e;
+        }
+    }*/
+
+    @Override
+    public EntityManager getEntityManager() {
+        return entityManager;
+    }
+}
