991ad79543283c016e8cad0311c36abfc96b4a65
Oscar Tan Guan <oscar.tan@uabc.edu.mx>
2025-09-13 01:59:14 +0100

    PBI-OTG-GAP-US1 Creación de AsignadoDAO

---
+++ persistencia/src/main/java/mx/desarrollo/dao/AsignadoDAO.java
@@ -0,0 +1,36 @@
+package mx.desarrollo.dao;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+import mx.desarollo.entity.Unidadaprendizaje;
+import mx.desarollo.persistence.AbstractDAO;
+
+import java.util.List;
+
+public class AsignadoDAO extends AbstractDAO<AsignadoDAO> {
+    private final EntityManager entityManager;
+
+    public AsignadoDAO(EntityManager em) {
+        super(AsignadoDAO.class);
+        this.entityManager = em;
+    }
+
+    public void asignar(AsignadoDAO asignacion) {
+        EntityTransaction tx = entityManager.getTransaction();
+        try {
+            tx.begin();
+            entityManager.persist(asignacion);
+            tx.commit();
+        } catch (Exception e) {
+            if (tx.isActive()) {
+                tx.rollback();
+            }
+            throw e;
+        }
+    }
+
+    @Override
+    public EntityManager getEntityManager() {
+        return entityManager;
+    }
+}
