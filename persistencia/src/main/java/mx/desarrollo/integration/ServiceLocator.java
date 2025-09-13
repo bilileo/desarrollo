80369aa7e0422d00d9a7b8d37337aa20ecc84127
Oscar Tan Guan <oscar.tan@uabc.edu.mx>
2025-09-13 02:01:13 +0100

    PBI-OTG-GAP-US1 Eliminación de un método que no corresponde en ServiceLocator, y agregar en ServiceLocator el método getInstanceAsignadoDAO

--- persistencia/src/main/java/mx/desarrollo/integration/ServiceLocator.java
+++ persistencia/src/main/java/mx/desarrollo/integration/ServiceLocator.java
@@ -13,7 +13,7 @@ import mx.desarrollo.persistence.HibernateUtil;
 public class ServiceLocator {
 
     private static UnidadAprendizajeDAO unidadAprendizajeDAO;
-
+    private static AsignadoDAO asignadoDAO;
     private static EntityManager getEntityManager(){
         return HibernateUtil.getEntityManager();
     }
@@ -30,18 +30,12 @@ public class ServiceLocator {
         }
     }
 
-    public static void guardarUnidadAprendizaje(Unidadaprendizaje ua) {
-        EntityManager em = getEntityManager();
-        try {
-            em.getTransaction().begin();
-            getInstanceUnidadAprendizajeDAO().guardar(ua);
-            em.getTransaction().commit();
-        } catch (Exception ex) {
-            if (em.getTransaction().isActive()) {
-                em.getTransaction().rollback();
-            }
-            throw ex; // relanzar o manejar el error
+    public static AsignadoDAO getInstanceAsignadoDAO(){
+        if(asignadoDAO== null){
+            asignadoDAO = new AsignadoDAO(getEntityManager());
+            return asignadoDAO;
+        } else{
+            return asignadoDAO;
         }
     }
-
 }
