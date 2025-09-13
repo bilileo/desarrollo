f56fceb5d0466c55c4f1ce79d7bedc14aff7447b
Oscar Tan Guan <oscar.tan@uabc.edu.mx>
2025-09-13 21:55:42 +0100

    PBI-OTG-GAP-US1 | Agregar el nuevo facade FacadeAsignacionUnidadProfesor al ServiceFacadeLocator.

--- negocio/src/main/java/mx/desarrollo/integration/ServiceFacadeLocator.java
+++ negocio/src/main/java/mx/desarrollo/integration/ServiceFacadeLocator.java
@@ -1,10 +1,12 @@
 package mx.desarrollo.integration;
 
+import mx.desarrollo.facade.FacadeAsignacionUnidadProfesor;
 import mx.desarrollo.facade.FacadeUnidadAprendizaje;
 
 public class ServiceFacadeLocator {
 
     private static FacadeUnidadAprendizaje facadeUnidadAprendizaje;
+    private static FacadeAsignacionUnidadProfesor facadeAsignacionUnidadProfesor;
 
     public static FacadeUnidadAprendizaje getInstanceFacadeUnidadAprendizaje() {
         if (facadeUnidadAprendizaje == null) {
@@ -12,6 +14,15 @@ public class ServiceFacadeLocator {
             return facadeUnidadAprendizaje;
         } else {
             return facadeUnidadAprendizaje;
+        }
+    }
+
+    public static FacadeAsignacionUnidadProfesor getInstanceFacadeAsignacionUnidadProfesor() {
+        if (facadeAsignacionUnidadProfesor == null) {
+            facadeAsignacionUnidadProfesor = new FacadeAsignacionUnidadProfesor();
+            return facadeAsignacionUnidadProfesor;
+        } else {
+            return facadeAsignacionUnidadProfesor;
         }
     }
 }
