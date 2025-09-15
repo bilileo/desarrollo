f602df7f56076906de42def8103ca1c47e5a70a2
Mariel <mariel.torres@uabc.edu.mx>
2025-09-14 21:39:26 -0700

    PBI-ALP-US1 capa de negocio terminada

--- negocio/src/main/java/mx/desarrollo/facade/FacadeProfesor.java
+++ negocio/src/main/java/mx/desarrollo/facade/FacadeProfesor.java
@@ -1,4 +1,17 @@
 package mx.desarrollo.facade;
 
+import mx.desarollo.entity.Profesor;
+import mx.desarrollo.delegate.DelegateProfesor;
+
 public class FacadeProfesor {
+    private final DelegateProfesor delegateProfesor;
+
+    public FacadeProfesor() {
+        this.delegateProfesor = new DelegateProfesor();
+    }
+
+    // Mandar prfoesor al delegate
+    public void guardarProfesor(Profesor profe){
+        delegateProfesor.saveProfesor(profe);
+    }
 }
